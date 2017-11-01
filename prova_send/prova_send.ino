4eb0dac84854c93638a2471c9a49df9aae915b64
masteruan <masteruan@gmail.com>
2017-11-01 23:49:05 +0100

    upgrade

--- prova_send/prova_send.ino
+++ prova_send/prova_send.ino
@@ -1,20 +1,12 @@
 /*
-  Serial Event example
-
- When new serial data arrives, this sketch adds it to a String.
- When a newline is received, the loop prints the string and
- clears it.
-
- A good test for this is to try it with a GPS receiver
- that sends out NMEA 0183 sentences.
-
- Created 9 May 2011
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/SerialEvent
-
+ * Receive the data from serial
+ *
+ * Write by i2c:
+ * Wire begin(); Set the channel for i2c
+ * Wire.beingTransmission(??); Declare the transmission channel
+ * Wire.write(1); Write the value
+ * Wire.endTransmission(); End transmission
+ *
  */
 #include <Wire.h>
 
@@ -27,6 +19,9 @@ void setup() {
   Serial.begin(9600);
   // reserve 200 bytes for the inputString:
   inputString.reserve(200);
+  Serial.println("###############");
+  Serial.println("You are Welcome");
+  Serial.flush();
 }
 
 void loop() {
@@ -34,31 +29,42 @@ void loop() {
   if (stringComplete) {
     Serial.println(inputString);
     //
-    if(inputString == "on\n"){
-      Wire.beginTransmission(8);
-      Wire.write(1);
-      Wire.endTransmission();
-      Serial.println("...inviato");
-    }
-    if(inputString = "off/n"){
-      Wire.beginTransmission(8);
-      Wire.write(0);
-      Wire.endTransmission();
-      Serial.println("...inviato");
-    }
+    seriale();
     // clear the string:
     inputString = "";
     stringComplete = false;
   }
 }
+void seriale (){
+     if(inputString == "on\n"){
+      Wire.beginTransmission(8);
+      Wire.write(1);
+      Wire.endTransmission();
+      Serial.println("...inviato");
+    }
+    if(inputString == "off\n"){
+      Wire.beginTransmission(8);
+      Wire.write(0);
+      Wire.endTransmission();
+      Serial.println("...inviato");
+    }
+     if(inputString == "onni\n"){
+      Wire.beginTransmission(11);
+      Wire.write(1);
+      Wire.endTransmission();
+      Serial.println("...inviato");
+    }
+    if(inputString == "offi\n"){
+      Wire.beginTransmission(11);
+      Wire.write(0);
+      Wire.endTransmission();
+      Serial.println("...inviato");
+    }
+}
 
-/*
-  SerialEvent occurs whenever a new data comes in the
- hardware serial RX.  This routine is run between each
- time loop() runs, so using delay inside loop can delay
- response.  Multiple bytes of data may be available.
- */
 void serialEvent() {
+  Serial.flush();
+  delay(20);
   while (Serial.available()) {
     // get the new byte:
     char inChar = (char)Serial.read();
